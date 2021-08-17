b56ec34f7cc5d24529f15681b23c25fc03ab7c81
Naoki206 <naoki020602060206@gmail.com>
2021-08-17 11:10:41 +0900

    unnecessary line removed

--- src/templates/blog-post-contentful.tsx
+++ src/templates/blog-post-contentful.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/destructuring-assignment */
 import * as React from 'react';
 import { graphql, PageProps } from 'gatsby';
 import { renderRichText } from 'gatsby-source-contentful/rich-text';
-import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { BLOCKS } from '@contentful/rich-text-types';
 
 import Bio from '../components/bio';
 import Layout from '../components/layout';
@@ -10,15 +11,16 @@ import Seo from '../components/seo';
 const options = {
   renderNode: {
     // eslint-disable-next-line react/display-name
-    [BLOCKS.EMBEDDED_ASSET]: node => {
-      console.log(node);
-      return (
-        <img
-          src={node.data.target.fixed.src}
-          alt={node.data.target.sys.title}
-        />
-      );
-    },
+    [BLOCKS.EMBEDDED_ASSET]: (node: {
+      data: {
+        target: {
+          fixed: { src: string | undefined };
+          sys: { title: string | undefined };
+        };
+      };
+    }) => (
+      <img src={node.data.target.fixed.src} alt={node.data.target.sys.title} />
+    ),
   },
 };
 
@@ -26,9 +28,6 @@ const BlogPostContentfulTemplate: React.FC<
   PageProps<GatsbyTypes.ContentfulBlogPostBySlugQuery>
 > = ({ data, location }) => {
   const post = data.contentfulPost;
-  // const siteTitle = data.site?.siteMetadata?.title || 'Title';
-  // const { previous, next } = data;
-
   return (
     <Layout location={location} title={post?.title || 'undefined'}>
       <Seo title={post?.title || 'undefined'} />
@@ -41,43 +40,13 @@ const BlogPostContentfulTemplate: React.FC<
           <h1 itemProp="headline">{post?.title}</h1>
           <p>{post?.date}</p>
         </header>
-        {/* <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        /> */}
-        {/* <div>{post?.content?.raw}</div> */}
+        {/* @ts-ignore */}
         <div>{post?.content?.raw && renderRichText(post.content, options)}</div>
         <hr />
         <footer>
           <Bio />
         </footer>
       </article>
-      {/* <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav> */}
     </Layout>
   );
 };
