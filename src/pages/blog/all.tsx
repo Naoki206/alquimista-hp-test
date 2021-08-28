a4f9ef90d154d4c4720adfbe51561195d22fab4c
Naoki206 <naoki020602060206@gmail.com>
2021-08-28 23:48:21 +0900

    card animation applied

--- src/pages/blog/all.tsx
+++ src/pages/blog/all.tsx
@@ -43,7 +43,7 @@ const BlogIndex: React.FC<PageProps<GatsbyTypes.BlogAllQuery>> = ({ data, locati
                       <Link to={`/blog/${post.node.slug}` || '/'} itemProp="url">
                         <div tw="overflow-hidden rounded-xl">
                           <img
-                            tw="w-full h-48 sm:h-48 lg:h-60 object-cover transform group-hover:scale-110"
+                            tw="w-full h-48 sm:h-48 lg:h-60 object-cover transform group-hover:scale-110 transition duration-300"
                             alt={post.node.image?.title}
                             src={post.node.image?.file?.url}
                           />
