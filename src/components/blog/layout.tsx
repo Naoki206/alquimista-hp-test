83f1ad17e2ed4bef7ae75d110061be4a42b3c908
Naoki206 <naoki020602060206@gmail.com>
2021-08-30 13:54:17 +0900

    responsive menu layout applied

--- src/components/blog/layout.tsx
+++ src/components/blog/layout.tsx
@@ -49,15 +49,16 @@ const Layout: React.FC<{ title: string } & { location: WindowLocation<unknown> }
   );
 
   const menu = data.site?.siteMetadata?.menu;
+  const [open, setOpen] = React.useState(false);
 
   return (
     <>
       <div tw="bg-paleOrange" data-is-root-path={isRootPath}>
-        <header tw="bg-darkBlue sm:h-16  sticky top-0 z-10">
-          <ul tw="mx-16 justify-end font-bold flex  text-xs sm:gap-2 md:gap-4 md:text-sm lg:gap-12 lg:text-base xl:gap-16 2xl:gap-20 h-full items-center list-none text-paleOrange">
+        <header tw="bg-darkBlue  sticky top-0 z-10">
+          <ul tw="mx-4 sm:mx-8 md:mx-16 justify-end font-bold flex text-xs md:gap-4 md:text-sm lg:gap-12 lg:text-base xl:gap-16 2xl:gap-20 h-16 items-center list-none text-paleOrange">
             <li tw="mr-auto">
               <img
-                tw="w-36"
+                tw="w-32 sm:w-36"
                 src="/alquimista_logo-02.svg"
                 alt="alquimista_logo-02"
                 className="hogehoge"
@@ -66,14 +67,60 @@ const Layout: React.FC<{ title: string } & { location: WindowLocation<unknown> }
             {/* <Link to="/" tw="text-paleOrange hidden sm:visible">
               <li>{menu?.aboutUs}</li>
             </Link> */}
-            <li tw="hidden sm:visible sm:flex">{menu?.aboutUs}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.vision}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.service}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.blog}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.news}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.member}</li>
-            <li tw="hidden sm:visible sm:flex">{menu?.contactUs}</li>
+            <li tw="md:hidden　transition duration-300">
+              {open ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  tw="w-8"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  onClick={() => setOpen(!open)}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  tw="w-8"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  onClick={() => setOpen(!open)}
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )}
+            </li>
+
+            <li tw="hidden md:block">{menu?.aboutUs}</li>
+            <li tw="hidden md:block">{menu?.vision}</li>
+            <li tw="hidden md:block">{menu?.service}</li>
+            <li tw="hidden md:block">{menu?.blog}</li>
+            <li tw="hidden md:block">{menu?.news}</li>
+            <li tw="hidden md:block">{menu?.member}</li>
+            <li tw="hidden md:block">{menu?.contactUs}</li>
           </ul>
+
+          {open && (
+            <ul tw="text-paleOrange p-5 list-none">
+              <li tw="py-5 border-b">{menu?.aboutUs}</li>
+              <li tw="py-5 border-b">{menu?.vision}</li>
+              <li tw="py-5 border-b">{menu?.service}</li>
+              <li tw="py-5 border-b">{menu?.blog}</li>
+              <li tw="py-5 border-b">{menu?.news}</li>
+              <li tw="py-5 border-b">{menu?.member}</li>
+              <li tw="inline-block mt-6 mb-4 py-1 px-5 rounded-full bg-red-400 text-darkBlue">
+                {menu?.contactUs}
+              </li>
+            </ul>
+          )}
         </header>
         <p tw="mb-0">{blogHeader}</p>
         <main>{children}</main>
