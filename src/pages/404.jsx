74103535e34feb4391908ab166e01a6c95d3c40a
Naoki206 <naoki020602060206@gmail.com>
2021-09-22 11:47:59 +0900

    404page applied

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import 'twin.macro';
 
 import Layout from '../components/layout';
@@ -11,8 +11,33 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle} blogOrNewsTopPage>
       <Seo title="404: Not Found" />
-      <div tw="h-screen flex pl-8 pt-3">
-        <h1>指定されたページは存在しません</h1>
+      <div tw="pb-80 h-auto w-4/5 sm:w-8/12 md:w-1/2 lg:w-2/5 xl:w-4/12  mr-auto ml-auto text-center">
+        <p tw="text-4xl md:text-5xl font-bold mt-16" style={{ fontFamily: 'Avenir Next' }}>
+          404 Not Found
+        </p>
+        <p tw="my-8 font-bold text-lg">お問い合わせフォーム</p>
+        <p tw="text-sm md:text-base">
+          申し訳ございません。ページが見つかりませんでした。
+          <br />
+          お探しのページはいずれかの理由により見つかりませんでした。
+        </p>
+        <div tw="text-center text-sm md:text-base">
+          <ul tw="text-left mr-auto ml-auto">
+            <li>ページのURLが変更された可能性があります。</li>
+            <li>ページが削除された可能性があります。</li>
+            <li>アドレス(URL)をタイプミスしている可能性があります。</li>
+          </ul>
+        </div>
+        <div tw="ml-auto mr-auto" style={{ fontFamily: 'Avenir Next' }}>
+          <Link to="/">
+            <button
+              type="button"
+              tw="my-12 block bg-darkBlue text-paleOrange mx-auto font-bold text-sm px-10 py-2 text-center border-2 border-paleOrange rounded-full hover:border-lightGreen hover:bg-lightGreen transition duration-300 outline-none"
+            >
+              TOP&nbsp;&nbsp;&nbsp;&nbsp;→
+            </button>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
